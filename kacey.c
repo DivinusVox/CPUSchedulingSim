486a09e0dd1cdfd73df075a1453502ea9bfa0433
Kacey Cole <kacey.purplestate.info>
2013-04-07 15:56:28 -0600

    stubbing functions in kacey.c

--- kacey.c
+++ kacey.c
@@ -1,11 +1,17 @@
-// First Come First Serv
+// First Come First Serve
+// takes array of ints
+// 	consumed in threes
 
-void FirstCome()
+void FirstCome(int* input)
 {
+	
 }
 
 
 // Shortest Remaining Time
-void ShortestRemaining()
+// takes array of ints,
+// 	consumed in threes
+void ShortestRemaining(int* input)
 {
+
 }
