be23a7f1ba6900923305daea4b8152575828b166
Kacey Cole <kacey.purplestate.info>
2013-04-07 16:18:33 -0600

    Testing stdin to tokenized strings

--- driver.c
+++ driver.c
@@ -2,14 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+
+#define LENGTH = 100
+
+ssize_t input;
 
 void main()
 {
+	int[3*LENGTH] values;
+	int i = 0;
 	// get stdinput
-	// fill arrays
+	while((input = read(STDIN_FILENO, buf, sizeof buf)) > 0)
+	{
+		// fill arrays
+		char* splitVals;
+		splitVals = strtok(input, " \t");
+		while(splitVals != NULL)
+		{
+			printf("%s\n", splitVals);
+			splitVals = strtok(input, " \t");			
+		}
+		i++;
+	}
 	
 	// do fcfs
 	// do shortest job
 	// do shortest remaining job
 	// do round robin
 }
+
+int[] getInput( FILE* inputFile)
